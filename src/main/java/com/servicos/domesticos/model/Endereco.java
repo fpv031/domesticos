01493262f4d149fd18cbdf2dd010ca63063ce6fa
fpv031 <feric5719@gmail.com>
2025-08-13 01:13:12 -0300

    ultima atualização erro em salvar dnv

--- src/main/java/com/servicos/domesticos/model/Endereco.java
+++ src/main/java/com/servicos/domesticos/model/Endereco.java
@@ -1,55 +1,44 @@
 package com.servicos.domesticos.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
+import java.lang.String;
 
 @Entity
 public class Endereco {
-
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String rua;
     private String numero;
+    private String bairro;
     private String cidade;
     private String estado;
     private String cep;
 
-    @ManyToOne
-    private Usuario usuario;
+    // Getters e setters
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
 
+    public String getRua() { return rua; }
+    public void setRua(String rua) { this.rua = rua; }
+
+    public String getNumero() { return numero; }
+    public void setNumero(String numero) { this.numero = numero; }
+
+    public String getBairro() { return bairro; }
+    public void setBairro(String bairro) { this.bairro = bairro; }
+
+    public String getCidade() { return cidade; }
+    public void setCidade(String cidade) { this.cidade = cidade; }
+
+    public String getEstado() { return estado; }
+    public void setEstado(String estado) { this.estado = estado; }
+
+    public String getCep() { return cep; }
+    public void setCep(String cep) { this.cep = cep; }
     public void setUsuario(Usuario orElseThrow) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'setUsuario'");
     }
-
-    public void setCep(String cep2) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setCep'");
-    }
-
-    public void setEstado(String estado2) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setEstado'");
-    }
-
-    public void setCidade(String cidade2) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setCidade'");
-    }
-
-    public void setNumero(String numero2) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setNumero'");
-    }
-
-    public void setRua(String rua2) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setRua'");
-    }
-
-    // Getters e Setters
 }
--- src/main/java/com/servicos/domesticos/model/Servico.java
+++ src/main/java/com/servicos/domesticos/model/Servico.java
@@ -1,11 +1,7 @@
 package com.servicos.domesticos.model;
 
-import java.util.List;  
+import java.util.List;
 import com.servicos.domesticos.model.PedidoServico;
-import com.servicos.domesticos.model.Endereco;
-import com.servicos.domesticos.model.Disponibilidade;
-import com.servicos.domesticos.model.Avaliacao;
-import com.servicos.domesticos.model.Servico;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -13,11 +9,11 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 
-
 @Entity
 public class Servico {
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String nome;
@@ -26,15 +22,16 @@ public class Servico {
     @OneToMany(mappedBy = "servico")
     private List<PedidoServico> pedidos;
 
-    public void setNome(String nome2) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setNome'");
-    }
-
-    public void setDescricao(String descricao2) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setDescricao'");
-    }
-
     // Getters e Setters
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+
+    public String getDescricao() { return descricao; }
+    public void setDescricao(String descricao) { this.descricao = descricao; }
+
+    public List<PedidoServico> getPedidos() { return pedidos; }
+    public void setPedidos(List<PedidoServico> pedidos) { this.pedidos = pedidos; }
 }
